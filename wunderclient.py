5747f8ec37a80a40d1660a1fd0dd7d1f6d30e6ee
Kevin LaFlamme <laflamme.kevin@gmail.com>
2017-03-23 07:47:55 -0400

    Refactor and start adding namedtuples for types

--- wunderclient.py
+++ wunderclient.py
@@ -1,7 +1,15 @@
 
+import json
+from collections import namedtuple
+from urllib import urlencode
 import requests
 
+# constants
 API_PREFIX = 'https://a.wunderlist.com/api/v1/'
+
+
+# types
+List = namedtuple('List', ['id', 'created_at', 'title', 'list_type', 'type', 'revision'])
 
 
 class WunderClient(object):
@@ -10,23 +18,76 @@ class WunderClient(object):
         self.client_id = client_id
         self.access_token = access_token
 
-    def request(self, path=None, method='GET'):
-        headers = {
-            'X-Client-ID': self.client_id,
-            'X-Access-Token': self.access_token
-        }
-        path = '{}{}'.format(API_PREFIX, path)
+    def _path(self, path=None, params=None):
+        full_path = '{}{}'.format(API_PREFIX, path)
+        if params is not None:
+            full_path = '{}?{}'.format(full_path, urlencode(params))
+        return full_path
 
-        if method == 'GET':
-            resp = requests.get(path, headers=headers)
-            resp.raise_for_status()
-            return resp.json()
+    def _headers(self):
+        return {
+            'X-Client-ID': self.client_id,
+            'X-Access-Token': self.access_token,
+            'Accept': 'application/json'
+        }
+
+    def _json(self, data):
+        if hasattr(data, '_asdict'):
+            ret = json.dumps(data._asdict())
+        else:
+            ret = json.dumps(data)
+        return ret
+
+    def _get(self, path=None, params=None):
+        resp = requests.get(self._path(path, params), headers=self._headers())
+        resp.raise_for_status()
+        return resp.json()
+
+    def _post(self, path=None, params=None):
+        headers = self._headers()
+        headers['Content-Type'] = 'application/json'
+        resp = requests.post(self._path(path), headers=headers, data=self._json(params))
+        resp.raise_for_status()
+        return resp.json()
+
+    def _delete(self, path=None, params=None):
+        resp = requests.delete(self._path(path, params), headers=self._headers())
+        resp.raise_for_status()
 
     def me(self):
-        return self.request('user')
+        return self._get('user')
 
     def get_lists(self):
-        return self.request('lists')
+        return self._get('lists')
 
-    def get_list(self, id):
-        return self.request('lists/{}'.format(id))
+    def get_list(self, id=None, title=None):
+        if id is None and title is None:
+            raise ValidationException('Must provide a `title` or `id`')
+
+        if id is None:
+            for l in self.get_lists():
+                if l.get('title') == title:
+                    id = l.get('id')
+                    break
+        if id is None:
+            raise ValidationException('List with title=`{}` does not exist'.format(title))
+
+        return self._get('lists/{}'.format(id))
+
+    def create_list(self, title):
+        if title is None:
+            raise ValidationException('\'title\' is required.')
+        elif not isinstance(title, basestring):
+            raise ValidationException('\'title\' must be a string')
+
+        return self._post('lists', {'title': title})
+
+    def delete_list(self, id=None, title=None):
+        if id is None:
+            l = self.get_list(title=title)
+            id = l['id']
+        self._delete('lists/{}'.format(id), params={'revision': l['revision']})
+
+
+class ValidationException(Exception):
+    pass
